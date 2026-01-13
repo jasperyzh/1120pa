888476bcffc049119d7410ca869a6f20e1aa440b
jasper.yzh <jasper.yzh@gmail.com>
2026-01-13 15:19:29 +0800

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-
-    site: 'https://1120pa.jasperyong.com',
+  site: "https://1120pa.jasperyong.com",
 });
